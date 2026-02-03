e376ed971402303378460fb219211724e54f123f
Hemanth Goud <hemanthgoudk24@gmail.com>
2026-02-03 11:43:13 +0530

    folder Structure added

---
+++ templete.sh
@@ -0,0 +1,15 @@
+# Creating directory 
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch app.py
+touch requirements.txt
+touch .env
+touch research/trials.ipynb
+
+echo "Directory and files created successfully."
